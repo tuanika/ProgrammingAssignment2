10a98ed3df96ed0e9f0655839e972faebbf58d1a
tuanika <kulikova.annae@gmail.com>
2014-12-16 22:32:03 +0100

    added commenting

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,42 @@
 ## These two functions calculate and cache the inverse of a matrix:
 
-## 'makeCacheMatrix' creates a special "matrix" object that can cache its inverse
+## 'makeCacheMatrix' creates a special "matrix" object that can cache 
+## inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) { # input x is a matrix
         
-        i <- NULL
-        set <- function(y) { 
+        i <- NULL #inverse is set to NULL everytime 'makeCacheMatrix' is called
+        
+        set <- function(y) { # input y is a matrix
                 x <<- y 
-                i <<- NULL 
+                i <<- NULL # inverse is reset to NULL if x changes
         }
-        get <- function() x
-        setinverse <- function(solve) i <<- solve 
-        getinverse <- function() i 
+        get <- function() {x} # returns the matrix
+        
+        setinverse <- function(solve) {i <<- solve}  
+        # sets inverse of a matrix, called by 'cacheSolve' at first access
+        
+        getinverse <- function() {i} 
+        # returns inverse of a matrix at the second access of cacheSolve
+        
         list(set = set, get = get, 
              setinverse = setinverse,
-             getinverse = getinverse)
+             getinverse = getinverse) # list of the internal functions
 }
 
 ## 'cacheSolve' returns inverse calculated from the special "matrix" returned by 
 ## 'makeCacheMatrix' or retrieves inverse from chache if it has been calculated
 cacheSolve <- function(x, ...) { 
         
-        i <- x$getinverse() # accesses the object x and gets its inverse
+        i <- x$getinverse() # accesses the object x and gets its inverse i
+        
         if(!is.null(i)) {  
+                
+                #if inverse exists it is returned
                 message("getting cached data")
                 return(i) 
         }
-        # calculate the inverse and store it back in x
+        
+        # if inverse is NULL then it is calculated, stored back in x and returned
         data <- x$get() 
         i <- solve(data, ...) 
         x$setinverse(i)
